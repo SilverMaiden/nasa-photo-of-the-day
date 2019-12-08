fd46605746262286240be26ca1a0577e481fd08d
SilverMaiden <h.specials@gmail.com>
2019-12-07 18:13:53 -0800

    removed unecessary component from nav bar

--- src/components/BasicComponent/Basic.js
+++ src/components/BasicComponent/Basic.js
@@ -37,23 +37,6 @@ export default class Basic extends React.Component {
               <NavItem>
                 <NavLink href="https://github.com/SilverMaiden/nasa-photo-of-the-day" target='_blank'>GitHub</NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
